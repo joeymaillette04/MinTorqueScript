24b5be5356315de0138239caff37f9769d1e6a37
camronsabahi <csabahip@uwaterloo.ca>
2023-03-31 19:39:38 -0400

    bug fix

--- testAlgorithm.py
+++ testAlgorithm.py
@@ -1,2 +1,32 @@
+import scipy.optimize._minimize
+import matplotlib.pyplot as plt 
+import numpy as np
+from script import *
+
+def runTests(approxHours):
+    num_trials = int(1014084*approxHours)
+
+    resultsLen = []
+    resultsTorque = []
+
+    for i in range(num_trials):
+        l1 = np.random.uniform(low=1.0, high=10.0, size=None)
+        l2 = np.random.uniform(low=1.0, high=10.0, size=None)
+        l3 = np.random.uniform(low=1.0, high=10.0, size=None)
+        testLengths = [l1, l2, l3]
+        results = scipy.optimize.minimize(calculateTorque2, testLengths, method="Nelder-Mead")
+        resultsLen.append(results.x)
+        resultsTorque.append(results.fun)
+
+    minTorqueIndex = 0
+
+
+    for i in range(num_trials):
+        if (resultsTorque[i] < resultsTorque[minTorqueIndex]):
+            minTorqueIndex = i
+
+    print(resultsLen[minTorqueIndex])
+    print(resultsTorque[minTorqueIndex])
 
 runTests(1/60/30)
+
