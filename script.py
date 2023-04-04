8c573e4d262d0e4ac099e21bfe79b67e1e2c143f
camronsabahi <csabahip@uwaterloo.ca>
2023-04-04 19:53:06 -0400

    fixed

--- script.py
+++ script.py
@@ -51,15 +51,16 @@ def mass( p, l ): #calculates mass of each arm based on length and its density
 def force( aMass ): #calculates force using mass and gravity constant (9.81N/kG)
     return aMass * 9.81
 
+
 def calculateTorque2( armLengths ):
-    gripperPts= [ [ 0.75, 0.1 ], [ 0.5, 0.5 ], [ 0.2, 0.6 ] ]
+    # gripperPts= [ [ 0.75, 0.1 ], [ 0.5, 0.5 ], [ 0.2, 0.6 ] ]
     gripperForce = 5 * 9.81
     gripperAngles = [ -math.pi/3.0 , 0, math.pi/4.0 ]
     torques = [ 0, 0, 0 ]
 
     for i in range( 3 ):
-        positionPoints[ i ][ 0 ][ 2 ] = gripperPts[ i ][ 0 ] - (armLengths[ 2 ] * abs(math.cos(gripperAngles[ i ])))
-        positionPoints[ i ][ 1 ][ 2 ] = gripperPts[ i ][ 1 ] - (armLengths[ 2 ] * math.sin(gripperAngles[ i ]))
+        positionPoints[ i ][ 0 ][ 2 ] = positionPoints[ i ][ 0 ][3] - (armLengths[ 2 ] * abs(math.cos(gripperAngles[ i ])))
+        positionPoints[ i ][ 1 ][ 2 ] = positionPoints[ i ][ 1 ][3] - (armLengths[ 2 ] * math.sin(gripperAngles[ i ]))
 
         midPts = [ [ ], [ ], [ ] ]
         positionPoints[i][0][1], positionPoints[i][1][1], x2, y2 = get_intersections(0, 0, armLengths[ 0 ], positionPoints[ i ][ 0 ][ 2 ], positionPoints[ i ][ 1 ][ 2 ], armLengths[ 1 ])
@@ -78,14 +79,14 @@ def calculateTorque2( armLengths ):
         #calculate the midpoints of each member, this is where the gravity force acts.
         midPts[ 0 ] = calcMidPt( 0, positionPoints[i][0][1] )
         midPts[ 1 ] = calcMidPt( positionPoints[i][0][1], positionPoints[i][0][2] )
-        midPts[ 2 ] = calcMidPt( positionPoints[i][0][2], gripperPts[i][0] )
+        midPts[ 2 ] = calcMidPt( positionPoints[i][0][2], positionPoints[i][0][3] )
 
         torque1 = gravForces[ 0 ] * calcMidPt( 0, positionPoints[i][0][1] ) + gravForces[ 1 ] * calcMidPt( positionPoints[i][0][1], positionPoints[i][0][2] )
         torque2 = gravForces[ 0 ] * calcMidPt( 0, x2 ) + gravForces[ 1 ] * calcMidPt( x2, positionPoints[i][0][2] )
 
         if (torque2 < torque1):
             positionPoints[i][0][1] = x2
-            positionPoints[i][1][1]
+            positionPoints[i][1][1] =y2
             midPts[0] = calcMidPt( 0, x2 )
             midPts[1] = calcMidPt( x2, positionPoints[i][0][2] )
 
@@ -93,28 +94,30 @@ def calculateTorque2( armLengths ):
         for j in range( 0, 3): #Add all the torque required for the three arms
             torques[ i ] += ( gravForces[ j ] * midPts[ j ] ) 
 
-        torques[ i ] += ( gripperForce * gripperPts[ i ][0] ) 
+        torques[ i ] += ( gripperForce * positionPoints[ i ][0][3] ) 
 
     finalTorque = math.sqrt( torques[ 0 ]**2 + torques[ 1 ]**2 + torques[ 2 ]**2 )
     print( "Position 1 torque", torques[0] )    
     return finalTorque
 
 testLengths = [ 0.4, 0.4, 0.3 ]
-# calculateTorque2(testLengths)
+fTorque = calculateTorque2(testLengths)
+print("Final combined torque", fTorque)
+
 
 
 # test = calculateTorque2(testLengths)
 # print("Torque of all positions combined", test)
 
 
-def lengthConstr( inputLens ):
-    return inputLens[0] + inputLens[1] + inputLens[2]
+# def lengthConstr( inputLens ):
+#     return inputLens[0] + inputLens[1] + inputLens[2]
 
-constrMax = NonlinearConstraint(lengthConstr, 1, np.Inf, keep_feasible=True)
+# constrMax = NonlinearConstraint(lengthConstr, 1, np.Inf, keep_feasible=True)
 
 
-def checkInter( inputLengths ):
-    return inputLengths[0] - inputLengths[1]
+# def checkInter( inputLengths ):
+#     return inputLengths[0] - inputLengths[1]
 
 cnstrnts = LinearConstraint( [ [ 1, 1, 1 ], [ -1, 1, 0 ] ], [ 1, 0 ], [ np.Inf, 0.1 ], keep_feasible=True )
 
@@ -122,17 +125,24 @@ cnstrnts = LinearConstraint( [ [ 1, 1, 1 ], [ -1, 1, 0 ] ], [ 1, 0 ], [ np.Inf, 
 bnds = Bounds( [ 0, 0, 0 ], [ 0.6, 0.5, 0.4 ], keep_feasible=True)
 
 
-results = scipy.optimize.minimize( calculateTorque2, testLengths, method="trust-constr", bounds=bnds, constraints=cnstrnts ) # options={'maxiter':1000})
+# results = scipy.optimize.minimize( calculateTorque2, testLengths, method="trust-constr", bounds=bnds, constraints=cnstrnts ) # options={'maxiter':1000})
 
-print(results)
+# print(results)
 
-finalLens = results.x
+# finalLens = results.x
 
 # print(finalLens[2])
 
-plt.xlim = 1
-plt.ylim = 1
+plt.xlim = 0.9
+plt.ylim = 0.9
 plt.grid()
+
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# ax.set_aspect('equal', adjustable='box')
+
+# plt.axes.Axes.set_aspect
+# plt.set_aspect('equal', adjustable='datalim') 
 
 plt.plot( positionPoints[0][0], positionPoints[0][1], label = "Pos 1")
 plt.plot( positionPoints[1][0], positionPoints[1][1], label = "Pos 2")
